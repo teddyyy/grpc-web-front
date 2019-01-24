72d2a33c7ee4e52a45a1f6e8f7337936f970985f
mz-kimoto <mz-kimoto@kddi.com>
2019-01-24 11:33:04 +0900

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ class App extends React.Component<{}, State> {
 
   private onClick = () => {
     const request = new HelloRequest();
-    let hoge = '';
     request.setName(this.state.inputText);
 
     const client = new GreeterClient(this.env.proxyURL, {}, {});
